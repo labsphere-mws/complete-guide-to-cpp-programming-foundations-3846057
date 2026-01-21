baf871a2981ee2155fbb88e256b655af383b7736
Marc S <58561530+labsphere-mws@users.noreply.github.com>
2026-01-21 19:25:05 +0000

    New Code. Plz Commit.

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,22 @@
 
 #include <iostream>
 
+int a, b = 5;
+
 int main(){
-    
+    bool dayTime;
+    a = 7;
+    dayTime = false;
+    std::cout << "a is: " << a << std::endl;
+    std::cout << "b is: " << b << std::endl;
+    std::cout << "dayTime is: " << dayTime << std::endl;
+    dayTime = true;
+    std::cout << "dayTime is: " << dayTime << std::endl;
+    std::cout << "a + b: " << a + b << std::endl;
+    std::cout << "b - a: " << b - a << std::endl;
+    unsigned int pos;
+    pos = b - a;
+    std::cout << "pos (b - a(unsigned)): " << pos << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
